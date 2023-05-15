8e0d9d49977cfd6a10b956af078a69c7f46cafdd
Christian Wesselhoeft <hi@xtian.us>
2023-05-15 12:02:22 -0600

    Add inheritance test

--- ts/test/Lox.test.ts
+++ ts/test/Lox.test.ts
@@ -1,10 +1,22 @@
-import { test, expect } from "bun:test";
+import { beforeAll, beforeEach, test, expect } from "bun:test";
 import Lox from "../Lox.js";
 
-test("fibonacci", async () => {
-  let result = "";
-  console.log = (...args) => (result += args.join(" ") + "\n");
+let result = "";
 
+beforeAll(() => {
+  console.log = (...args) => (result += args.join(" ") + "\n");
+});
+
+beforeEach(() => {
+  result = "";
+});
+
+test("fibonacci", async () => {
   await Lox.main([`${__dirname}/fibonacci.lox`]);
+  expect(result).toMatchSnapshot();
+});
+
+test("inheritance", async () => {
+  await Lox.main([`${__dirname}/inheritance.lox`]);
   expect(result).toMatchSnapshot();
 });
